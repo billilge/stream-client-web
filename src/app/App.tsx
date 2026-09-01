1e535ce1ba1b24fcd6bc034b038ec99cde680131
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-09-02 02:30:09 +0900

    chore: Tailwind CSS v4 도입

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <main>
-      <h1>STREAM</h1>
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="font-bold text-3xl">STREAM</h1>
     </main>
   );
 }
