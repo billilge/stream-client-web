fd612c39cfd615aa46733a871000000934b90bd0
sangrae <tiger030205@gmail.com>
2026-09-12 01:06:12 +0900

    feat: 행사 신청 화면 조립과 라우트 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import ScreenLayout from "@/components/ui/ScreenLayout";
 import BililgeListScreen from "@/features/bililge/BililgeListScreen";
+import EventsApplicationScreen from "@/features/events/EventsApplicationScreen";
 import HomeScreen from "@/features/home/HomeScreen";
 
 // 데스크톱에서 보기 좋게 아이폰 화면 크기로 가운데 정렬만 해준다.
@@ -12,6 +13,13 @@ function App() {
         <Route element={<ScreenLayout />}>
           <Route element={<HomeScreen />} path="/" />
           <Route element={<BililgeListScreen />} path="/bililge" />
+        </Route>
+        {/* Bottom Nav 대신 하단 고정 버튼(Action Area)이 있는 화면 */}
+        <Route element={<ScreenLayout hasBottomNav={false} />}>
+          <Route
+            element={<EventsApplicationScreen />}
+            path="/events/:eventId/apply"
+          />
         </Route>
       </Routes>
     </div>
